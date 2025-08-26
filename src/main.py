aadb58d3003420edb613600c3a27dcd97a8ab20a
Mohammadreza Naseri <themohammadreza2006@gmail.com>
2025-08-26 15:05:43 +0330

    Create Project Structure

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import random
+
+class TicTacToe:
+    def __init__(self):
+        pass
+
+    def get_random_first_player(self):
+        pass
+
+    def player_movement(self, cell, player):
+        pass
+
+    def has_anyone_won(self, player):
+        pass
+
+    def swap_player_turn(self):
+        pass
+
+    def is_board_filled(self):
+        pass
+
+    def start_game(self):
+        pass
+
+if __name__ == "__main__":
+    pass
